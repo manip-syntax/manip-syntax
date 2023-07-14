8181938aa7434368d543a0031ec415235d1163dd
Philippe Sergent <philippe.sergent@ac-poitiers.fr>
2023-07-14 14:15:12 +0200

    On peut maintenant utiliser le menu déroulant pour les nouvelles fonctions

--- src/nouvelle_phrase.ts
+++ src/nouvelle_phrase.ts
@@ -1,6 +1,6 @@
 import { affiche_phrase } from './affichage_phrase';
 import { Fonction, PhraseCorrigee, PhraseEleve } from './phrase';
-import { non_null } from './util';
+import { byID, non_null } from './util';
 import { fonctions_communes } from './fonctions_partagees';
 
 // Nouvelle phrase: bouton ok
@@ -13,6 +13,11 @@ let fonction_de_validation_de_la_phrase = () => console.log("Problème: aucune f
 non_null(document.getElementById("bouton-valider-phrase")).addEventListener('click', () => {
     fonction_de_validation_de_la_phrase();
 });
+let enregistre_fonction = () => {
+    console.log("Problème: enregistre_fonction n'a pas été instancié");
+    return new PhraseEleve("vide",new PhraseCorrigee("vide"));
+}
+
 
 
 const fonctions_choix : { [nom: string] : Fonction } = {
@@ -25,12 +30,12 @@ const fonctions_choix : { [nom: string] : Fonction } = {
     "Attribut du sujet" : "attribut_du_sujet",
     "Attribut du COD" : "attribut_du_cod",
     "Complément d'agent" : "complément_d_agent",
-    "Complément circonstanciel" : "complément_circonstanciel",
+    //"Complément circonstanciel" : "complément_circonstanciel",
     "Complément du verbe impersonnel" : "complément_du_verbe_impersonnel",
-    "Modalisateur" : "modalisateur",
-    "Fonction auto-énonciative" : "auto-énonciative",
-    "Connecteur" : "connecteur",
-    "Balise textuelle" : "balise_textuelle"
+    //"Modalisateur" : "modalisateur",
+    //"Fonction auto-énonciative" : "auto-énonciative",
+    //"Connecteur" : "connecteur",
+    //"Balise textuelle" : "balise_textuelle"
 };
 
 function analyse_de_fonction(pos:number, phrase: PhraseEleve): void {
@@ -38,7 +43,7 @@ function analyse_de_fonction(pos:number, phrase: PhraseEleve): void {
         non_null(document.getElementById("phrase-analyse-paragraphe")).innerHTML = affiche_phrase(phrase);
         non_null(document.getElementById("consigne-container")).innerHTML = `À renseigner : ${nom}`;
 
-        const enregistre_fonction = () => {
+        enregistre_fonction = () => {
             const mots_selectionnes = Array.from(document.getElementsByClassName("phrase-selectionne"))
                               .map(elt => Number(elt.id.split('-')[2]));
             phrase.declareFonction(fonction, mots_selectionnes);
@@ -49,7 +54,7 @@ function analyse_de_fonction(pos:number, phrase: PhraseEleve): void {
             const filename = "phrase.{{numéro_de_version}}.json";
 
             // TODO ajout de la version dans le fichier json?
-            const blob = new Blob([JSON.stringify(phrase)], { type: "text/json" });
+            const blob = new Blob([JSON.stringify(enregistre_fonction())], { type: "text/json" });
             const lien = document.createElement('a');
             lien.download = filename;
             lien.href = window.URL.createObjectURL(blob);
@@ -67,6 +72,10 @@ function analyse_de_fonction(pos:number, phrase: PhraseEleve): void {
         fonctions_communes.fonction_de_validation = () => {
             const npos = pos === Object.entries(fonctions_choix).length - 1 ? 0 : pos + 1;
             analyse_de_fonction(npos, enregistre_fonction());
+            // sélecteur
+            const selecteur = byID("nouvelle_phrase-fonctions-selection") as HTMLSelectElement;
+            // BUG: si on sélectionne depuis le sélecteur, c'est le mauvais index qui est choisi
+            selecteur.selectedIndex = npos;
         };
 }
 
@@ -105,6 +114,15 @@ export function nouvelle_phrase() : void {
         let nouvelle_phrase = new PhraseEleve(nouveau_texte, new PhraseCorrigee(nouveau_texte));
         modal_nouvelle_phrase.style.display = 'none';
         analyse_de_fonction(0, nouvelle_phrase);
+
+        // Evénement pour le sélecteur
+        byID("nouvelle_phrase-fonctions-selection").addEventListener('change', e => {
+            const target = e.target as HTMLSelectElement;
+            // enregistrement
+            enregistre_fonction();
+            // chargement de la fonction désirée
+            analyse_de_fonction(target.selectedIndex, nouvelle_phrase );
+        });
         
 
 
