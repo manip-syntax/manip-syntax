8cff7aa1348cb728775b684297384620dac13396
Philippe Sergent <philippe.sergent@ac-poitiers.fr>
2023-07-21 21:15:39 +0200

    Refonte de l'organisation de la nouvelle phrase

--- src/nouvelle_phrase.ts
+++ src/nouvelle_phrase.ts
@@ -6,39 +6,21 @@ import './nouvelle_phrase.css';
 
 // Nouvelle phrase: bouton ok
 let fonction_du_bouton_de_nouvelle_phrase = () => console.log("Problème: aucune fonction définie pour le bouton OK de la nouvelle phrase");
-non_null(document.getElementById("modal-nouvelle_phrase-bouton")).addEventListener('click', () => {
+byID("modal-nouvelle_phrase-bouton").addEventListener('click', () => {
     fonction_du_bouton_de_nouvelle_phrase();
 });
 // Nouvelle phrase: bouton validation de la phrase
 let fonction_de_validation_de_la_phrase = () => console.log("Problème: aucune fonction définie pour la validation de la nouvelle phrase");
-non_null(document.getElementById("bouton-valider-phrase")).addEventListener('click', () => {
+byID("bouton-valider-phrase").addEventListener('click', () => {
     fonction_de_validation_de_la_phrase();
 });
 let enregistre_fonction = () => {
     console.log("Problème: enregistre_fonction n'a pas été instancié");
-    return new PhraseEnveloppe("vide");
+    return new CreateurPhrase("vide");
 }
 
 
 
-const fonctions_choix : { [nom: string] : Fonction } = {
-    "Sujet" : "sujet",
-    "Verbes" : "verbes",
-    "Noyau" : "noyau",
-    "Verbe principal" : "verbe_principal",
-    "Groupe verbal" : "groupe_verbal",
-    "COD" : "cod",
-    "COI" : "coi",
-    "Attribut du sujet" : "attribut_du_sujet",
-    "Attribut du COD" : "attribut_du_cod",
-    "Complément d'agent" : "complement_d_agent",
-    "Complément circonstanciel-1" : "complement_circonstanciel",
-    "Complément du verbe impersonnel" : "complement_du_verbe_impersonnel",
-    "Modalisateur-1" : "modalisateur",
-    "Fonction auto-énonciative-1" : "auto-enonciative",
-    "Connecteur-1" : "connecteur",
-    "Balise textuelle-1" : "balise_textuelle"
-};
 
 
 class _fonction_tracee {
@@ -52,8 +34,23 @@ class _fonction_tracee {
         this.nom = nom;
     }
 }
+class FonctionTracee {
+    public validee: boolean = false;
+    public changee: boolean = false;
+    public est_multiple: boolean = false;
+    constructor(public nom_de_base: string, public fonction: Fonction, public numero: number = -1) {
+        this.est_multiple = numero > -1;
+    }
 
-class PhraseEnveloppe {
+    get nom(): string {
+        if (this.numero > -1) {
+            return `${this.nom_de_base}-${this.numero + 1}`;
+        }
+        return this.nom_de_base;
+    }
+}
+
+class CreateurPhrase {
 
     private _phrase: PhraseEleve;
     private _fonctions_multiples_traceur : { [nom: string] : _fonction_tracee } = {
@@ -63,14 +60,69 @@ class PhraseEnveloppe {
         "auto-enonciative" : new _fonction_tracee("auto-enonciative"),
         "connecteur" : new _fonction_tracee("connecteur")
     };
+    private _traceur: FonctionTracee[] = [];
+    private _pos: number = 0;
+    private _selecteur: HTMLSelectElement = byID("nouvelle_phrase-fonctions-selection") as HTMLSelectElement;
+    static liste_des_fonctions: { [nom: string] : Fonction } = {
+        "Sujet" : "sujet",
+        "Verbes" : "verbes",
+        "Noyau" : "noyau",
+        "Verbe principal" : "verbe_principal",
+        "Groupe verbal" : "groupe_verbal",
+        "COD" : "cod",
+        "COI" : "coi",
+        "Attribut du sujet" : "attribut_du_sujet",
+        "Attribut du COD" : "attribut_du_cod",
+        "Complément d'agent" : "complement_d_agent",
+        "Complément circonstanciel" : "complement_circonstanciel",
+        "Complément du verbe impersonnel" : "complement_du_verbe_impersonnel",
+        "Modalisateur" : "modalisateur",
+        "Fonction auto-énonciative" : "auto-enonciative",
+        "Connecteur" : "connecteur",
+        "Balise textuelle" : "balise_textuelle"
+    };
+
+    constructor(texte: string) {
+        this._phrase = new PhraseEleve(texte, new PhraseCorrigee(texte));
+        Object.entries(CreateurPhrase.liste_des_fonctions).forEach(
+            elt => this.ajouter_fonction_tracee(this._traceur.length, elt[0], elt[1])
+        );
+        this._selecteur.style.display = 'block';
+    }
+
+    set pos(i: number) {
+        /* Cette fonction permet de modifier la position dans le CreateurPhrase
+         * mais aussi dans le sélecteur
+         */
+        assert(i < this._traceur.length,`${i} est plus grand que la longueur du traceur: ${this._traceur.length}.`);
+        this._pos = i;
+        assert(i < this._selecteur.options.length,`${i} est plus grand que la longueur du sélecteur ${this._selecteur.options.length}.`);
+        this._selecteur.selectedIndex = i;
+    }
+
+    get fonction_courante(): FonctionTracee {
+        return this._traceur[this._pos];
+    }
+
+    
+    ajouter_fonction_tracee(pos: number, nom: string, fonction: Fonction, numero:number = -1) {
+        /* Ajouter une fonction dans le traceur et dans le selecteur
+         * pos correspond à l'emplacement dans la liste
+         */
+        if (PhraseEleve.Fonctions_multiples.includes(fonction) && numero === -1) {
+            numero = 0;
+        }
+        assert(pos <= this._traceur.length,`${pos} est plus grand que la longueur du traceur`);
+        assert(pos <= this._selecteur.options.length, `${pos} est plus grand que la longueur du selecteur: ${this._selecteur.options.length}`);
+        const f = new FonctionTracee(nom, fonction, numero);
+        this._traceur.splice(pos, 0, f);
+        this._selecteur.add(new Option(f.nom, f.fonction), pos);
+    }
 
     _verifie_existence_fonction(nom: Fonction) {
         assert(nom in this._fonctions_multiples_traceur,"${nom} n'est pas une fonction multiple");
     }
 
-    constructor(texte: string) {
-        this._phrase = new PhraseEleve(texte, new PhraseCorrigee(texte));
-    }
 
     get phrase(): PhraseEleve {
         return this._phrase;
@@ -108,32 +160,20 @@ class PhraseEnveloppe {
         this._verifie_existence_fonction(nom);
         return this._fonctions_multiples_traceur[nom].changee;
     }
-}
 
-const selecteur = byID("nouvelle_phrase-fonctions-selection") as HTMLSelectElement;
-function ajout_element_selecteur(nom: string, f: Fonction, numero: number) {
-    const current_pos = selecteur.selectedIndex;
-    nom = nom.split("-")[0];
-    selecteur.add( new Option(`${nom}-${numero+2}`, f), selecteur.options[current_pos+1]);
-}
-
-
-
-function analyse_de_fonction(pos:number, phrase: PhraseEnveloppe): void {
-        const nom = selecteur.options[pos].text;
-        const fonction = selecteur.options[pos].value as Fonction;
-        non_null(document.getElementById("phrase-analyse-paragraphe")).innerHTML = affiche_phrase(phrase.phrase);
-        non_null(document.getElementById("consigne-container")).innerHTML = `À renseigner : ${nom}`;
-        // édition de la fonction?
-        // // quelle fonction exactement?
-        let numero = -1;
-        if (PhraseEleve.Fonctions_multiples.includes(fonction)) {
-            numero = parseInt(nom.split("-")[1]) - 1;
-            console.log("numero",numero);
-            phrase.fm_pos_set(fonction, numero);
+    analyse_de_fonction(pos: number = -1): void {
+        if (pos === -1) {
+            pos = this._pos;
+        } else {
+            this.pos = pos;
         }
-        //// selection des mots précédemment sélectionnés
-        const mots_selectionnes = phrase.phrase.fonctionPos(fonction, numero);
+        const nom = this.fonction_courante.nom;
+        const fonction = this.fonction_courante.fonction;
+        byID("phrase-analyse-paragraphe").innerHTML = affiche_phrase(this._phrase);
+        byID("consigne-container").innerHTML = `À renseigner : ${nom}`;
+
+        // selection des mots précédemment sélectionnés
+        const mots_selectionnes = this._phrase.fonctionPos(fonction, this.fonction_courante.numero);
         Array.from(document.getElementsByClassName("phrase-cliquable"))
             .forEach( elt => {
                 if (mots_selectionnes.includes(Number(elt.id.split('-')[2]))) {
@@ -141,28 +181,23 @@ function analyse_de_fonction(pos:number, phrase: PhraseEnveloppe): void {
                 }
             });
 
-
-
-
         enregistre_fonction = () => {
             const mots_selectionnes = Array.from(document.getElementsByClassName("phrase-selectionne"))
                               .map(elt => Number(elt.id.split('-')[2]));
-            const numero_de_fonction = PhraseEleve.Fonctions_multiples.includes(fonction) ? phrase.fm_pos(fonction) : -1;
-            if (PhraseEleve.Fonctions_multiples.includes(fonction)) {
-                if (phrase.phrase.fonctions_multiples_nombre(fonction) === phrase.fm_pos(fonction)) {
+            if (this.fonction_courante.est_multiple) {
+                if (this.phrase.fonctions_multiples_nombre(fonction) === this.fonction_courante.numero) {
                     if (mots_selectionnes.length === 0) {
-                    // si c'est une fonction multiple et que rien n'a été enregistré, on passe
-                    phrase.fm_changer(fonction, false);
-                    return phrase;
+                        // si c'est une fonction multiple et que rien n'a été enregistré, on passe
+                        return this;
                     } else {
                         // nouvelle fonction multiple
-                        ajout_element_selecteur(nom, fonction, phrase.fm_pos(fonction));
-                        phrase.fm_ajouter(fonction);
+                        this.ajouter_fonction_tracee(pos + 1, this.fonction_courante.nom_de_base, fonction, this.fonction_courante.numero + 1);
                     }
                 }
             }
-            phrase.phrase.declareFonction(fonction, mots_selectionnes, numero_de_fonction);
-            return phrase;
+            this._phrase.declareFonction(fonction, mots_selectionnes, this.fonction_courante.numero);
+            this.fonction_courante.validee = true;
+            return this;
         }
 
         fonction_de_validation_de_la_phrase = () => {
@@ -187,12 +222,14 @@ function analyse_de_fonction(pos:number, phrase: PhraseEnveloppe): void {
 
         fonctions_communes.fonction_de_validation = () => {
             enregistre_fonction();
-            const npos = pos += 1;
-            analyse_de_fonction(npos, phrase);
-            // sélecteur
-            selecteur.selectedIndex = npos;
+            this.analyse_de_fonction(pos + 1);
         };
 }
+}
+
+
+
+
 
 export function nouvelle_phrase() : void {
     // Disparition des autres modals
@@ -200,35 +237,26 @@ export function nouvelle_phrase() : void {
         modal.style.display = "none";
     }
 
-    const modal_nouvelle_phrase = non_null(document.getElementById("modal-nouvelle-phrase"));
+    const modal_nouvelle_phrase = byID("modal-nouvelle-phrase");
     modal_nouvelle_phrase.style.display = "block";
-    // remplissage du choix des  fonctions
-    let options = "";
-    Object.entries(fonctions_choix).forEach(
-        ([nom, fonction, ]) => {
-            options += `<option value="${fonction}">${nom}</option>\n`;
-        });
-    const liste_fonctions = non_null(document.getElementById("nouvelle_phrase-fonctions-selection"));
-    liste_fonctions.innerHTML = options;
-    liste_fonctions.style.display = 'block';
     // Divers éléments à afficher
-    non_null(document.getElementById("conseil")).innerHTML = "Sélectionnez chaque fonction comme si vous étiez vous-même l'élève. Cliquez sur valider quand vous avez terminé votre sélection. Valider dans rien sélectionner indique que cette fonction est absente de la phrase. Vous pouvez toujours corriger une éventuelle erreur en sélectionnant une fonction dans la liste déroulante.";
-    const bouton_valider = non_null(document.getElementById("bouton-valider"));
+    byID("conseil").innerHTML = "Sélectionnez chaque fonction comme si vous étiez vous-même l'élève. Cliquez sur valider quand vous avez terminé votre sélection. Valider dans rien sélectionner indique que cette fonction est absente de la phrase. Vous pouvez toujours corriger une éventuelle erreur en sélectionnant une fonction dans la liste déroulante.";
+    const bouton_valider = byID("bouton-valider");
     bouton_valider.style.width = "50%";
     bouton_valider.innerHTML = "Valider la fonction";
     // validation de la phrase
-    const valider_phrase = non_null(document.getElementById("bouton-valider-phrase"));
+    const valider_phrase = byID("bouton-valider-phrase");
     valider_phrase.style.display = "block";
 
     fonction_du_bouton_de_nouvelle_phrase = () => {
-        let nouveau_texte = non_null(document.getElementById("nouvelle_phrase-texterea") as HTMLTextAreaElement).value;
+        let nouveau_texte = (byID("nouvelle_phrase-texterea") as HTMLTextAreaElement).value;
         if (nouveau_texte.trim() === '') {
             // rien n'a été entré
             return;
         }
-        let nouvelle_phrase = new PhraseEnveloppe(nouveau_texte);
+        let nouvelle_phrase = new CreateurPhrase(nouveau_texte);
         modal_nouvelle_phrase.style.display = 'none';
-        analyse_de_fonction(0, nouvelle_phrase);
+        nouvelle_phrase.analyse_de_fonction();
 
         // Evénement pour le sélecteur
         byID("nouvelle_phrase-fonctions-selection").addEventListener('change', e => {
@@ -236,11 +264,8 @@ export function nouvelle_phrase() : void {
             // enregistrement
             enregistre_fonction();
             // chargement de la fonction désirée
-            analyse_de_fonction(target.selectedIndex, nouvelle_phrase );
+            nouvelle_phrase.analyse_de_fonction(target.selectedIndex);
         });
-        
-
-
     };
     
 }
