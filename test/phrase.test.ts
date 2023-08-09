0bd69faa135e01188a84e24e0a168841bc37e98e
Philippe Sergent <philippe.sergent@ac-poitiers.fr>
2023-08-09 07:05:42 +0200

    Refonte: tests

--- test/phrase.test.ts
+++ test/phrase.test.ts
@@ -1,30 +1,30 @@
 import {describe, expect, test } from '@jest/globals';
-import { GroupeEnchasse, MotsPos, Phrase, PhraseCorrigee, PhraseEleve } from '../src/phrase';
+import { MotsPos, Syntagme, SyntagmeCorrige, SyntagmeEleve } from '../src/phrase';
 
 
-describe('Phrase class tests', () => {
-  test('Phrase est correctement découpée en mots', () => {
-    let p = new Phrase("L'ours a-t-il mangé du miel ?");
+describe('Syntagme class tests', () => {
+  test('Syntagme est correctement découpée en mots', () => {
+    let p = new Syntagme("L'ours a-t-il mangé du miel ?");
     expect(p.longueur).toBe(8);
     expect(p.phraseCassee).toEqual("L ours a t il mangé du miel".split(" "));
   });
 
-  test("Phrase renvoie bien son contenu", () => {
-      let p = new Phrase("Le chat mange la souris.");
-      let pc = new PhraseCorrigee("L'éléphant joue dans le magasin de porcelaine.");
-      let pe = new PhraseEleve("Je dors.",pc);
+  test("Syntagme renvoie bien son contenu", () => {
+      let p = new Syntagme("Le chat mange la souris.");
+      let pc = new SyntagmeCorrige("L'éléphant joue dans le magasin de porcelaine.");
+      let pe = new SyntagmeEleve("Je dors.",pc);
       expect(p.contenu).toBe("Le chat mange la souris.");
       expect(pe.contenu).toBe("Je dors.");
       expect(pc.contenu).toBe("L'éléphant joue dans le magasin de porcelaine.");
   });
 
-  test('Phrase déclare les bonnes fonctions', () => {
-      let p = new Phrase("Le chat a mangé la souris.");
+  test('Syntagme déclare les bonnes fonctions', () => {
+      let p = new Syntagme("Le chat a mangé la souris.");
       p.declareFonction("sujet",[0,1]);
       expect(p.fonctionPos("sujet")).toEqual([0,1]);
       expect(p.fonctionMots("sujet")).toBe("Le chat");
 
-      let p2 = new Phrase("L'ordinateur a-t-il été rangé ?");
+      let p2 = new Syntagme("L'ordinateur a-t-il été rangé ?");
       let verbe_pos: MotsPos = [2, 5, 6];
       p2.declareFonction("verbe_noyau", verbe_pos);
       p2.declareFonction("verbes",verbe_pos);
@@ -36,7 +36,7 @@ describe('Phrase class tests', () => {
       expect(() => { p2.fonctionPos("balise_textuelle",-1);}).toThrow();
       expect(() => { p2.fonctionPos("complement_circonstanciel");}).toThrow();
 
-      let p3 = new Phrase("Il vécut à Rome dans l'Antiquité.");
+      let p3 = new Syntagme("Il vécut à Rome dans l'Antiquité.");
       const cc1: MotsPos = [2,3];
       const cc2: MotsPos = [4, 5, 6];
       p3.declareFonction("complement_circonstanciel",cc1);
@@ -65,8 +65,8 @@ describe('Phrase class tests', () => {
 
   });
 
-  test('PhraseCorrigee vérifie que tel mot a bien telle fonction', () => {
-      let p = new PhraseCorrigee("La terre a tressailli d'un souffle prophétique");
+  test('SyntagmeCorrige vérifie que tel mot a bien telle fonction', () => {
+      let p = new SyntagmeCorrige("La terre a tressailli d'un souffle prophétique");
       const verbe_pos: MotsPos = [2, 3];
       p.declareFonction("verbe_noyau",verbe_pos);
       expect(p.estFonction("verbe_noyau",[2,3])).toBe(true);
@@ -78,14 +78,14 @@ describe('Phrase class tests', () => {
 
   });
 
-  test("PhraseCorrigee prend correctement json en charge", () => {
-      let p = new PhraseCorrigee("Le chat mange la souris.");
+  test("SyntagmeCorrige prend correctement json en charge", () => {
+      let p = new SyntagmeCorrige("Le chat mange la souris.");
       p.declareFonction("verbes",[2]);
       p.declareFonction("verbe_noyau",[2]);
       p.declareFonction("sujet",[0,1]);
       p.declareFonction("cod",[3,4]);
       let p_json_copie = JSON.stringify(p);
-      let p_copie = PhraseCorrigee.fromJSON(p_json_copie);
+      let p_copie = SyntagmeCorrige.fromJSON(p_json_copie);
       expect(p_copie.estFonction("verbe_noyau",[2])).toBe(true);
       expect(p_copie.estFonction("sujet",[0,1])).toBe(true);
       expect(p_copie.estFonction("cod",[3,4])).toBe(true);
@@ -93,21 +93,21 @@ describe('Phrase class tests', () => {
 
   });
 
-  test('PhraseEleve déclare une fonction et on lui répond si c est juste', () => {
+  test('SyntagmeEleve déclare une fonction et on lui répond si c est juste', () => {
       const phrase = "Mon luth constellé porte le soleil noir de la mélancolie.";
-      let corrige = new PhraseCorrigee(phrase);
+      let corrige = new SyntagmeCorrige(phrase);
       corrige.declareFonction("verbe_noyau",[3]);
       corrige.declareFonction("sujet",[0,1,2]);
       corrige.declareFonction("cod",[4,5,6,7,8,9]);
-      let phrase_eleve = new PhraseEleve(phrase, corrige);
+      let phrase_eleve = new SyntagmeEleve(phrase, corrige);
       expect(phrase_eleve.declare("verbe_noyau",[0])).toBe(false);
       expect(phrase_eleve.fonctionMots("verbe_noyau")).toBe("Mon");
       expect(phrase_eleve.declare("verbe_noyau",[3])).toBe(true);
   });
 
-  test("Phrase trouve la bonne fonction pour un numéro donné.", () => {
+  test("Syntagme trouve la bonne fonction pour un numéro donné.", () => {
       const phrase = "Les sanglots longs des violons de l'automne blessent mon coeur d'une langueur motone";
-      let p = new Phrase(phrase);
+      let p = new Syntagme(phrase);
       // l'index doit être inférieur à la longueur de la phrase
       expect(p.fonction(phrase.length)).toEqual([]);
       p.declareFonction("verbes",[8]);
@@ -122,16 +122,16 @@ describe('Phrase class tests', () => {
 
       // fonctions multiples
       const texte = "Il vit en Italie, à Rome.";
-      let pp = new Phrase(texte);
+      let pp = new Syntagme(texte);
       pp.declareFonction("complement_circonstanciel",[2,3]);
       pp.declareFonction("complement_circonstanciel",[4,5]);
       expect(pp.fonction(3)).toEqual(["complement_circonstanciel"]);
       expect(pp.fonction(4)).toEqual(["complement_circonstanciel"]);
   });
 
-  test("Phrase trouve la bonne fonction enchassée pour un numéro donné.", () => {
+  test("Syntagme trouve la bonne fonction enchassée pour un numéro donné.", () => {
       const phrase = "Le petit enfant mange.";
-      let p = new Phrase(phrase);
+      let p = new Syntagme(phrase);
       p.declareFonction("sujet",[0,1,2]);
       let gn_sujet = p.cree_groupe_enchasse([0,1,2],"sujet",-1);
       gn_sujet.declareFonction("noyau",[2]);
@@ -141,9 +141,9 @@ describe('Phrase class tests', () => {
       expect(p.fonction(0)).toEqual(["sujet"]);
   });
 
-  test("Phrase trouve la bonne fonction détaillée pour une position donnée", () => {
+  test("Syntagme trouve la bonne fonction détaillée pour une position donnée", () => {
       const phrase = "Les sanglots longs des violons de l'automne blessent mon coeur d'une langueur motone";
-      let p = new Phrase(phrase);
+      let p = new Syntagme(phrase);
       // l'index doit être inférieur à la longueur de la phrase
       expect(p.fonction(phrase.length)).toEqual([]);
       p.declareFonction("verbes",[8]);
@@ -157,15 +157,15 @@ describe('Phrase class tests', () => {
       expect(p.fonction_detaillee(3)).toEqual([["sujet",0,7]]);
       expect(p.fonction_detaillee(10)).toEqual([["cod",9,10]]);
 
-      let p2 = new Phrase("Il vécut à Paris en 1801.");
+      let p2 = new Syntagme("Il vécut à Paris en 1801.");
       p2.declareFonction("complement_circonstanciel",[2,3]);
       p2.declareFonction("complement_circonstanciel",[4,5]);
       expect(p2.fonction_detaillee(4)).toEqual([["complement_circonstanciel",4,5]]);
   });
 
-  test("Phrase trouve la bonne fonction détaillée dans le bon ordre pour un numéro donné, enchassements compris", () => {
+  test("Syntagme trouve la bonne fonction détaillée dans le bon ordre pour un numéro donné, enchassements compris", () => {
       const phrase = "L'enfant qui travaille est assis";
-      let p = new Phrase(phrase);
+      let p = new Syntagme(phrase);
       p.declareFonction("sujet",[0,1,2,3]);
       let gn_sujet = p.cree_groupe_enchasse([0,1,2,3], "sujet",-1);
       gn_sujet.declareFonction("noyau",[1]);
@@ -177,7 +177,7 @@ describe('Phrase class tests', () => {
   });
 
   test("Corrige sait si la phrase contient telle ou telle fonction", () => {
-      let phrase = new PhraseCorrigee("La critique est aisée.");
+      let phrase = new SyntagmeCorrige("La critique est aisée.");
       phrase.declareFonction("verbe_noyau",[2]);
       phrase.declareFonction("sujet",[0,1]);
       phrase.declareFonction("attribut_du_sujet",[3]);
@@ -187,8 +187,8 @@ describe('Phrase class tests', () => {
       expect(phrase.aFonction("cod")).toBe(false);
   });
 
-  test("GroupeEnchasse est bien noté vide quand il l'est, et non s'il est rempli", () => {
-      let ge = new GroupeEnchasse([0,2,3,4]);
+  test("Syntagme est bien noté vide quand il l'est, et non s'il est rempli", () => {
+      let ge = new Syntagme("Une phrase sans importance",[0,2,3,4]);
       expect(ge.vide).toBe(true);
 
       // vide avec un groupe enchasse vide
@@ -200,13 +200,13 @@ describe('Phrase class tests', () => {
       expect(ge.vide).toBe(false);
 
       // plein avec une fonction multiple
-      let ge2 = new GroupeEnchasse([0,1,2,3]);
+      let ge2 = new Syntagme("",[0,1,2,3]);
       ge2.declareFonction("complement_circonstanciel",[0,1],0);
       expect(ge2.vide).toBe(false);
   });
 
-  test("Phrase renvoie les bons groupes enchassés et les supprime", () => {
-      let phrase = new Phrase("Le matin, le petit chaperon rouge entra dans le bois où se trouvait un grand méchant loup.");
+  test("Syntagme renvoie les bons groupes enchassés et les supprime", () => {
+      let phrase = new Syntagme("Le matin, le petit chaperon rouge entra dans le bois où se trouvait un grand méchant loup.");
       const [cc1, cc2] = [[0,1], [7,8,9,10,11,12,13,14,15,16]];
       phrase.cree_groupe_enchasse(cc1, "complement_circonstanciel",0);
       phrase.cree_groupe_enchasse(cc2, "complement_circonstanciel",1);
@@ -218,8 +218,8 @@ describe('Phrase class tests', () => {
 
   });
 
-  test("Phrase copie correctement", () => {
-      let sa = new Phrase("Peu importe le contenu pourvu qu'il soit assez long pour correspondre aux numéros des fonctions déclarées.");
+  test("Syntagme copie correctement", () => {
+      let sa = new Syntagme("Peu importe le contenu pourvu qu'il soit assez long pour correspondre aux numéros des fonctions déclarées.");
       sa.declareFonction("sujet",[0,1,2]);
       sa.declareFonction("complement_circonstanciel",[3,4,5],0);
       sa.declareFonction("complement_circonstanciel",[6,7,8],1);
@@ -239,12 +239,12 @@ describe('Phrase class tests', () => {
       expect(sa.copie.groupes_enchasses_nombre).toBe(1);
   });
 
-  test("PhraseEleve et PhraseCorrigee gèrent correctement les syntagmes multiples", () => {
+  test("SyntagmeEleve et SyntagmeCorrige gèrent correctement les syntagmes multiples", () => {
       const phrase = "Hier, j'étais à Paris.";
-      let pc = new PhraseCorrigee(phrase);
+      let pc = new SyntagmeCorrige(phrase);
       pc.declareFonction("complement_circonstanciel",[0]);
       pc.declareFonction("complement_circonstanciel",[3,4]);
-      let pe = new PhraseEleve(phrase, pc);
+      let pe = new SyntagmeEleve(phrase, pc);
       expect(pc.estFonction("complement_circonstanciel",[1])).toBe(false);
       expect(pe.declare("complement_circonstanciel",[1],0)).toBe(false);
       expect(pe.declare("complement_circonstanciel",[0],0)).toBe(true);
@@ -255,13 +255,13 @@ describe('Phrase class tests', () => {
   test("cree_groupe_enchasse fonctionne correctement", () => {
       const texte = "Je fais ce que je veux.";
 
-      let pc = new PhraseCorrigee(texte);
+      let pc = new SyntagmeCorrige(texte);
       pc.cree_groupe_enchasse([2,3,4,5], "cod", -1);
       let gec = pc.groupe_enchasse("cod",-1);
       gec.declareFonction("complement_du_pronom",[3,4,5]);
       gec.declareFonction("noyau",[2]);
 
-      let pe = new PhraseEleve(texte, pc);
+      let pe = new SyntagmeEleve(texte, pc);
       let gee = pe.cree_groupe_enchasse_eleve(gec, "cod",-1);
       expect(gee.declare("complement_du_pronom",[3,4,5], 0)).toBe(true);
 
@@ -269,13 +269,13 @@ describe('Phrase class tests', () => {
 
   test("Les groupes enchâssés sont correctement sérialisés en JSON", () => {
       let texte = "L'âne de Buridan mourut de faim et de soif.";
-      let p = new Phrase(texte);
+      let p = new Syntagme(texte);
       let g = p.cree_groupe_enchasse([0,1,2,3], "sujet",-1);
       g.declareFonction("noyau",[1]);
       g.declareFonction("complement_du_nom",[2,3])
 
       const json = JSON.stringify(p);
-      const pp = PhraseCorrigee.fromJSON(json);
+      const pp = SyntagmeCorrige.fromJSON(json);
       const gp = pp.groupe_enchasse("sujet",-1);
       expect(gp.mots_pos).toEqual([0,1,2,3]);
       expect(gp.fonctionPos("noyau")).toEqual([1]);
@@ -284,7 +284,7 @@ describe('Phrase class tests', () => {
   });
 
   test("La profondeur est d'un groupe enchâssé est correcte", () => {
-      let g = new GroupeEnchasse([0,1,2,3,4]);
+      let g = new Syntagme("Une phrase sans importance.",[0,1,2,3,4]);
       expect(g.profondeur).toBe(0);
       let g2 = g.cree_groupe_enchasse([2,3,4], "cod",-1);
       expect(g.profondeur).toBe(1);
@@ -299,7 +299,7 @@ describe('Phrase class tests', () => {
 
   test("Les mots sans fonction sont bien renvoyés à la demande", () => {
       const base = [0,1,2,3,4,5,6];
-      let g = new GroupeEnchasse(base);
+      let g = new Syntagme("Une phrase sans importance.",base);
       expect(g.mots_sans_fonction).toEqual(base);
       g.declareFonction("sujet",[0,1,2,3]);
       expect(g.mots_sans_fonction).toEqual([4,5,6]);
@@ -309,7 +309,7 @@ describe('Phrase class tests', () => {
   });
 
   test("Un syntagme quelconque est bien reconnu attributif ou non selon le cas", () => {
-      let g = new GroupeEnchasse([0,1,2,3]);
+      let g = new Syntagme("Une phrase sans importance.",[0,1,2,3]);
       expect(g.est_attributif).toBe(false);
       g.declareFonction("sujet",[0]);
       expect(g.est_attributif).toBe(false);
